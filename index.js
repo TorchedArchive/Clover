dae8fac9987609c7cc2c078d16a9d2ebd445e9b4
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-07-11 18:37:45 -0400

    always return array if value is given

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ module.exports = (_args) => {
 	const args = _args.split("-").filter(t => t).map(t => t.trim().split(" "))
 	let parsed = {}
 	for(let i = 0; i < args.length; i++) {
-		parsed[args[i][0]] = args[i][1];
-		
-		if(!args[i][1]) parsed[args[i][0]] = true;
-		if(args[i].length > 2) parsed[args[i][0]] = args[i].slice(1);
+		!args[i][1] ? parsed[args[i][0]] = true : parsed[args[i][0]] = args[i].slice(1)
 	}
-	return parsed;
+	return parsed
 }
