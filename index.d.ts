6281ffe3da17ace48a2d34e2ad977c2ba04a0c22
Noel <augu@voided.pw>
2020-07-12 19:51:32 -0700

    Code updates, fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 declare module '@terminalfreaks/clover' {
-  type Clover<T extends object> = (args: string) => { [x in keyof T]: T };
-  export = Clover;
+  type Clover<T extends object> = { [P in keyof T]?: T[P][]; };
+  function clover<T extends object>(args: string): Clover<T>;
+
+  export = clover;
 }
